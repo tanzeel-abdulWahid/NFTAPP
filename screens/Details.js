7e18dffe99e6b8c0b87f170883c3bb20527c4dd6
Tanzeel Abdul Wahid <tanzeelwahid111@gmail.com>
2022-06-14 00:53:15 +0500

    done

--- screens/Details.js
+++ screens/Details.js
@@ -58,6 +58,16 @@ const Details = ({route, navigation}) => {
 
                     <View style={{padding: SIZES.font}}>
                         <DetailsDesc data={data} />
+
+                        {data.bids.length > 0 && (
+                            <Text style={{
+                                fontSize:SIZES.font,
+                                fontFamily:FONTS.semiBold,
+                                color:COLORS.primary
+                            }}>
+                                Current bids
+                            </Text>
+                        )}
                     </View>
                 </React.Fragment>
             )}
